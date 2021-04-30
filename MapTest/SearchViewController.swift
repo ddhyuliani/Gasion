5009349cd044438d11f1245077a3dec62865727a
ddhyuliani <ddhyuliani@gmail.com>
2021-04-30 20:58:06 +0700

    initial

---
+++ MapTest/SearchViewController.swift
@@ -0,0 +1,103 @@
+//
+//  SearchViewController.swift
+//  MapTest
+//
+//  Created by Dian Dinihari on 30/04/21.
+//
+
+import UIKit
+import CoreLocation
+
+protocol SearchViewControllerDelegate: AnyObject {
+    func searchViewController(_ vc: SearchViewController, didSelectLocationWith coordinates: CLLocationCoordinate2D?)
+}
+
+class SearchViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    weak var delegate: SearchViewControllerDelegate?
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Get estimation"
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        return label
+    }()
+    
+    private let field: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Enter destination"
+        field.layer.cornerRadius = 8
+        field.backgroundColor = .tertiarySystemBackground
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        field.leftViewMode = .always
+        return field
+    }()
+    
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .secondarySystemBackground
+        view.addSubview(label)
+        view.addSubview(field)
+        view.addSubview(tableView)
+        field.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .secondarySystemBackground
+    
+    }
+    
+    var locations = [Location]()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        label.sizeToFit()
+        label.frame = CGRect(x: 10, y: 10, width: label.frame.size.width, height: label.frame.size.height)
+        field.frame = CGRect(x: 10, y: 20+label.frame.size.height, width: view.frame.size.height-20, height: 50)
+        let tableY: CGFloat = field.frame.origin.y+field.frame.size.height+5
+        tableView.frame = CGRect(x: 0, y: tableY, width: view.frame.size.width, height: view.frame.size.height-tableY)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        field.resignFirstResponder()
+        if let text = field.text, !text.isEmpty {
+            LocationManager.shared.findLocation(with: text) {[weak self] location in
+                DispatchQueue.main.async {
+                    self?.locations = location
+                    self?.tableView.reloadData()
+                }
+            }
+        }
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = locations[indexPath.row].title
+        cell.textLabel?.numberOfLines = 0
+        cell.contentView.backgroundColor = .secondarySystemBackground
+        cell.backgroundColor = .secondarySystemBackground
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        print("check if any in tableview")
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Notify Map controller to show pin at selected place
+        let coordinate = locations[indexPath.row].coordinates
+        
+        delegate?.searchViewController(self, didSelectLocationWith: coordinate)
+    }
+ 
+}
--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import MapKit
+import FloatingPanel
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SearchViewControllerDelegate  {
+    
+    let mapView = MKMapView()
+    let panel = FloatingPanelController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(mapView)
+        title = ""
+        
+        let searchVC = SearchViewController()
+        searchVC.delegate = self
+        panel.set(contentViewController: searchVC)
+        panel.addPanel(toParent: self)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapView.frame = view.bounds
     }
 
-
+    func searchViewController(_ vc: SearchViewController, didSelectLocationWith coordinates: CLLocationCoordinate2D?) {
+        print(coordinates as Any)
+        
+        // Make sure we have valid coordinates
+        guard let existingCoordinates = coordinates else {
+            return
+        }
+        // dismiss the panel after searching and zooming to pinpoint
+        panel.move(to: .tip, animated: true)
+        
+        // remove all pin if any
+        mapView.removeAnnotations(mapView.annotations)
+        
+        // make a new pin point on map
+        let pin = MKPointAnnotation()
+        pin.coordinate = existingCoordinates
+        mapView.addAnnotation(pin)
+        
+        mapView.setRegion(MKCoordinateRegion(
+            center: existingCoordinates,
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.8,
+                longitudeDelta: 0.8
+            )
+        ),
+        animated: true)
+    }
 }
 
